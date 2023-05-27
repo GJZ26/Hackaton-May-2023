31f38a9a63097911d64732be1157d91f3460c24d
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-05-27 10:11:05 -0600

    feat(script):Gravity, and colision

--- script/Player.js
+++ script/Player.js
@@ -1,7 +1,11 @@
 export default class Player{
+    gravitySpeed = 0.5;
     constructor(canvas, x , y ,speed, width, height, color){
         this.canvas = canvas;
-        this.speed = speed;
+        this.speed ={
+            x: 0,
+            y: 0.1
+        }
         this.width = width;
         this.height = height;
         this.color = color;
@@ -20,7 +24,14 @@ export default class Player{
         this.context.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
     gravity(){
-        this.position.y += 1.9
+        this.position.y += this.speed.y;
+        if(this.position.y + this.height +this.speed.y < this.canvas.height){
+            this.speed.y += this.gravitySpeed;
+        }
+        else{
+            this.speed.y = 0;
+        }
+        
     }
     clear(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
--- script/main.js
+++ script/main.js
@@ -3,7 +3,7 @@ const canvas = document.createElement("canvas")
 document.body.appendChild(canvas)
 canvas.style.backgroundColor = "gray"
 
-const player1 = new Player(canvas, 0,0, 1.8, 200, 200, "red")
+const player1 = new Player(canvas, 0,0, 3.8, 100, 100, "red")
 
 
 function config() {
