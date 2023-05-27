2aa8788e9d791b7737589744e1842899719d263a
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-05-27 09:42:29 -0600

    feat(script): Gravity and player

--- script/main.js
+++ script/main.js
@@ -1,19 +1,24 @@
+import Player from './Player.js';
 const canvas = document.createElement("canvas")
 document.body.appendChild(canvas)
 canvas.style.backgroundColor = "gray"
+
+const player1 = new Player(canvas, 0,0, 1.8, 200, 200, "red")
 
 
 function config() {
     canvas.width = window.innerWidth - 1
     canvas.height = window.innerHeight - 1
-
     requestAnimationFrame(update)
 }
 
 function update() {
     // Animation loop
-
     requestAnimationFrame(update)
+    player1.gravity();
+    player1.clear();
+    player1.draw();
+
 }
 
 config()
